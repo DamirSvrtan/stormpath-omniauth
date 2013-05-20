fefd219c43f171e6a8ab676236fc9cfeb7ce867c
Erin Swenson-Healey <erin@carbonfive.com>
2013-05-20 14:11:07 -0700

    Adding web mock and VCR as dev dependency
    - configure VCR and RSpec in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,17 @@
+require 'vcr'
+require 'webmock'
+
 ENV["RAILS_ENV"] ||= 'test'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+end
 
 require File.expand_path("../dummy/config/environment", __FILE__)
 
